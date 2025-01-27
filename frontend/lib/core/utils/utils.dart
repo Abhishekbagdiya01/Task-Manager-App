e3f810153b87fa3979a6d5c2586bbf4496795f3c
Abhishek bagdiya <abhishekbagdiya94@gmail.com>
2025-01-27 13:17:32 +0530

    Fix some bug in date selecter

--- frontend/lib/core/utils/utils.dart
+++ frontend/lib/core/utils/utils.dart
@@ -1,7 +1,7 @@
 List<DateTime> generateWeekDates(int weekOffSet) {
-  DateTime today = DateTime.now();
+  final DateTime today = DateTime.now();
   DateTime startOfWeek = today.subtract(Duration(days: today.weekday - 1));
-  startOfWeek.add(Duration(days: weekOffSet * 7));
+  startOfWeek = startOfWeek.add(Duration(days: weekOffSet * 7));
   return List.generate(
     7,
     (index) => startOfWeek.add(Duration(days: index)),
--- frontend/lib/features/home/screens/date.selecter.dart
+++ frontend/lib/features/home/screens/date.selecter.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:frontend/core/utils/utils.dart';
 import 'package:intl/intl.dart';
@@ -10,18 +12,23 @@ class DateSelecter extends StatefulWidget {
 }
 
 class _DateSelecterState extends State<DateSelecter> {
+  DateTime selectedDate = DateTime.now();
   int weekOffSet = 0;
   @override
   Widget build(BuildContext context) {
     final weekDates = generateWeekDates(weekOffSet);
-    String monthName = DateFormat("MMMM").format(weekDates.first);
+    final String monthName = DateFormat("MMMM").format(weekDates.first);
     return Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    weekOffSet--;
+                  });
+                },
                 icon: const Icon(
                   Icons.arrow_back_ios,
                   size: 30,
@@ -31,7 +38,11 @@ class _DateSelecterState extends State<DateSelecter> {
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    weekOffSet++;
+                  });
+                },
                 icon: const Icon(
                   Icons.arrow_forward_ios,
                   size: 30,
@@ -39,22 +50,50 @@ class _DateSelecterState extends State<DateSelecter> {
           ],
         ),
         SizedBox(
-          height: 80,
+          height: 85,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: weekDates.length,
             itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.all(5),
-                width: 50,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  weekDates[index].day.toString(),
-                  style: const TextStyle(
-                      fontSize: 22, fontWeight: FontWeight.bold),
+              DateTime date = weekDates[index];
+              bool isSelected = DateFormat('d').format(selectedDate) ==
+                      DateFormat('d').format(date) &&
+                  selectedDate.month == date.month &&
+                  selectedDate.year == date.year;
+              return InkWell(
+                onTap: () {
+                  selectedDate = date;
+                  log("$selectedDate == $date");
+                  setState(() {});
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.all(5),
+                  width: 75,
+                  decoration: BoxDecoration(
+                      color: isSelected ? Colors.redAccent : Colors.white,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    children: [
+                      Text(
+                        weekDates[index].day.toString(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: isSelected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                      Text(
+                        DateFormat("E").format(weekDates[index]),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: isSelected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               );
             },
